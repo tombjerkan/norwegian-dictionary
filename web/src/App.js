8b8dfe5ce84fd4acfc0e4abb82a00039b8832506
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-07-18 16:42:14 +0100

    Map API response to unstyled HTML in front-end

--- web/src/App.js
+++ web/src/App.js
@@ -15,10 +15,50 @@ class App extends Component {
     render() {
         return (
           <div>
-              {this.state.response}
+              {this.state.response !== null && this.state.response.map(entry => <Entry entry={entry} />)}
           </div>
         );
     }
+}
+
+function Entry({ entry }) {
+    return (
+        <div>
+            <h1>{entry.term}</h1>
+
+            <div>
+                <h2>{entry.definition.header}</h2>
+
+                {entry.definition.interpretations.length > 0 &&
+                    <ol>
+                        {entry.definition.interpretations.map(interpretation =>
+                            <li>
+                                <h3>{interpretation.header}</h3>
+
+                                <div>{interpretation.examples}</div>
+
+                                {interpretation.expanded &&
+                                    <div>
+                                        <div>{interpretation.expanded.header}</div>
+                                        <div>{interpretation.expanded.examples}</div>
+                                    </div>
+                                }
+
+                                {interpretation.articleContent.length > 0 &&
+                                    <ul>
+                                        {interpretation.articleContent.map(articleEntry =>
+                                            <li>
+                                                {articleEntry.header} {articleEntry.expanded}
+                                            </li>
+                                        )}
+                                    </ul>}
+                            </li>
+                        )}
+                    </ol>
+                }
+            </div>
+        </div>
+    )
 }
 
 export default App;

b5b89bf5261c56212a205474258200dccfc1e9b0
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-07-19 17:11:42 +0100

    Basic styling of resulting HTML

--- web/src/App.js
+++ web/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styles from "./App.module.css";
 
 class App extends Component {
 
@@ -14,8 +15,12 @@ class App extends Component {
 
     render() {
         return (
-          <div>
-              {this.state.response !== null && this.state.response.map(entry => <Entry entry={entry} />)}
+            <div className={styles.container}>
+                {this.state.response !== null &&
+                    <ul className={styles.entriesList}>
+                        {this.state.response.map(entry => <Entry entry={entry} />)}
+                    </ul>
+                }
           </div>
         );
     }
@@ -23,41 +28,41 @@ class App extends Component {
 
 function Entry({ entry }) {
     return (
-        <div>
-            <div>{entry.term}</div>
+        <li>
+            <h3 className={styles.term}>{entry.term}</h3>
 
-            <div>
-                <div>{entry.etymology}</div>
+            <div className={styles.etymology}>{entry.etymology}</div>
 
-                {entry.senses.length > 0 &&
-                    <ol>
-                        {entry.senses.map(sense =>
-                            <li>
-                                <div>{sense.definition}</div>
+            {entry.senses.length > 0 &&
+                <ol className={styles.sensesList}>
+                    {entry.senses.map(sense =>
+                        <li>
+                            <div className={styles.definition}>{sense.definition}</div>
 
-                                <div>{sense.examples}</div>
+                            <div className={styles.examples}>{sense.examples}</div>
 
-                                {sense.subDefinition &&
-                                    <div>
-                                        <div>{sense.subDefinition.definition}</div>
-                                        <div>{sense.subDefinition.examples}</div>
-                                    </div>
-                                }
+                            {sense.subDefinition &&
+                                <div className={styles.subDefinitionContainer}>
+                                    <div className={styles.subDefinition}>{sense.subDefinition.definition}</div>
+                                    <div className={styles.subDefinitionExamples}>{sense.subDefinition.examples}</div>
+                                </div>
+                            }
 
-                                {sense.subEntries.length > 0 &&
-                                    <ul>
-                                        {sense.subEntries.map(subEntry =>
-                                            <li>
-                                                {subEntry.term} {subEntry.definition}
-                                            </li>
-                                        )}
-                                    </ul>}
-                            </li>
-                        )}
-                    </ol>
-                }
-            </div>
-        </div>
+                            {sense.subEntries.length > 0 &&
+                                <ul className={styles.subEntriesList}>
+                                    {sense.subEntries.map(subEntry =>
+                                        <li>
+                                            <span className={styles.subEntryTerm}>{subEntry.term}</span>
+                                            {" "}
+                                            {subEntry.definition}
+                                        </li>
+                                    )}
+                                </ul>}
+                        </li>
+                    )}
+                </ol>
+            }
+        </li>
     )
 }
 

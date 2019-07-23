849574888a2d68e4362cc1e2ddd2cfeb6107f288
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-07-23 15:19:52 +0100

    Put google auth values in environment variables

--- server/fetchFromGoogleTranslate.js
+++ server/fetchFromGoogleTranslate.js
@@ -1,6 +1,11 @@
 const { TranslationServiceClient } = require('@google-cloud/translate').v3beta1;
 
-const translationClient = new TranslationServiceClient();
+const translationClient = new TranslationServiceClient({
+    credentials: {
+        client_email: process.env.GOOGLE_AUTH_CLIENT_EMAIL,
+        private_key: process.env.GOOGLE_AUTH_PRIVATE_KEY
+    }
+});
 
 async function fetchFromGoogleTranslate(word) {
     const [response] = await translationClient.translateText({
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require("express");
 const fetchFromOrdbok = require("./fetchFromOrdbok");
 const fetchFromWiktionary = require("./fetchFromWiktionary");

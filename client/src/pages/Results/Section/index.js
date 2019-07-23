9a885207a65590a63c77945d9df526fb81f89379
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-07-24 00:32:39 +0100

    Add button to show/hide section content

--- client/src/pages/Results/Section/index.js
+++ client/src/pages/Results/Section/index.js
@@ -4,6 +4,7 @@ import Loading from "components/Loading";
 
 function Section({ name, title, query, render }) {
     const [data, setData] = useState(null);
+    const [isOpen, setOpen] = useState(false);
 
     useEffect(() => {
         fetch(`/api/${name}/${query}`)
@@ -11,10 +12,23 @@ function Section({ name, title, query, render }) {
             .then(data => setData(data));
     }, [name, query]);
 
+    function toggleOpen() {
+        setOpen(!isOpen);
+    }
+
     return (
         <div className={styles.container}>
-            <h2>{title}</h2>
-            {data !== null ? render(data) : <Loading />}
+            <h2 className={styles.title}>{title}</h2>
+
+            {data !== null ? (
+                <React.Fragment>
+                    <button onClick={toggleOpen}>{isOpen ? "Hide" : "Show"}</button>
+
+                    {isOpen && <div className={styles.content}>{render(data)}</div>}
+                </React.Fragment>
+             ) : (
+                <Loading />
+             )}
         </div>
     )
 }

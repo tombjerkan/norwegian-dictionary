f29567d62f16cd9aeabc12e4ed44a0674bb35361
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-07-23 20:08:28 +0100

    Rename 'web' directory to 'client'

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const api = require("./api");
 const app = express();
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.resolve(__dirname, "./web/build")));
+    app.use(express.static(path.resolve(__dirname, "./client/build")));
 }
 
 app.use("/api", api);
 
 if (process.env.NODE_ENV === "production") {
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "./web/build", "index.html")));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "./client/build", "index.html")));
 }
 
 let port = process.env.PORT || 8080;

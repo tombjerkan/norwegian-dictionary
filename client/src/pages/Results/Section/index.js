0399cd108e08071439742c746cb90e4851c27a89
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-07-24 15:41:48 +0100

    Show errors on results page

--- client/src/pages/Results/Section/index.js
+++ client/src/pages/Results/Section/index.js
@@ -4,12 +4,29 @@ import Loading from "components/Loading";
 
 function Section({ name, title, query, render }) {
     const [data, setData] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+    const [isError, setError] = useState(false);
     const [isOpen, setOpen] = useState(false);
 
     useEffect(() => {
+        setLoading(true);
+
         fetch(`/api/${name}/${query}`)
-            .then(response => response.json())
-            .then(data => setData(data));
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else if (response.status === 404) {
+                    return null;
+                } else {
+                    setError(true);
+                    return null;
+                }
+            })
+            .then(data => {
+                setData(data);
+                setLoading(false);
+            })
+            .catch(() => setError(true));
     }, [name, query]);
 
     function toggleOpen() {
@@ -20,15 +37,17 @@ function Section({ name, title, query, render }) {
         <div className={styles.container}>
             <h2 className={styles.title}>{title}</h2>
 
-            {data !== null ? (
+            {data !== null && (
                 <React.Fragment>
                     <button onClick={toggleOpen}>{isOpen ? "Hide" : "Show"}</button>
 
                     {isOpen && <div className={styles.content}>{render(data)}</div>}
                 </React.Fragment>
-             ) : (
-                <Loading />
              )}
+
+            {data === null && !isLoading && !isError && <div>Not available</div>}
+            {isLoading && <Loading />}
+            {isError && <div>Error</div>}
         </div>
     )
 }

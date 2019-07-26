54472522c35284473b37eaa9302fd4aa12ccc84a
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-07-26 14:20:10 +0100

    Split Entry component into parts

--- client/src/pages/Results/Ordbok/index.js
+++ client/src/pages/Results/Ordbok/index.js
@@ -19,40 +19,66 @@ function Entry({ entry }) {
     return (
         <li>
             <h3 className={styles.term}>{entry.term}</h3>
-
             <div className={styles.etymology}>{entry.etymology}</div>
-
-            {entry.senses.length > 0 &&
-                <ol className={styles.sensesList}>
-                    {entry.senses.map(sense =>
-                        <li>
-                            <div className={styles.definition}>{sense.definition}</div>
-
-                            <div className={styles.examples}>{sense.examples}</div>
-
-                            {sense.subDefinition &&
-                                <div className={styles.subDefinitionContainer}>
-                                    <div className={styles.subDefinition}>{sense.subDefinition.definition}</div>
-                                    <div className={styles.subDefinitionExamples}>{sense.subDefinition.examples}</div>
-                                </div>
-                            }
-
-                            {sense.subEntries.length > 0 &&
-                                <ul className={styles.subEntriesList}>
-                                    {sense.subEntries.map(subEntry =>
-                                        <li>
-                                            <span className={styles.subEntryTerm}>{subEntry.term}</span>
-                                            {" "}
-                                            {subEntry.definition}
-                                        </li>
-                                    )}
-                                </ul>}
-                        </li>
-                    )}
-                </ol>
-            }
+            <Senses senses={entry.senses} />
         </li>
     )
+}
+
+function Senses({ senses }) {
+    if (senses.length === 0) {
+        return null;
+    }
+
+    return (
+        <ol className={styles.sensesList}>
+            {senses.map(sense =>
+                <li>
+                    <div className={styles.definition}>{sense.definition}</div>
+                    <div className={styles.examples}>{sense.examples}</div>
+                    <SubDefinition subDefinition={sense.subDefinition} />
+                    <SubEntries subEntries={sense.subEntries} />
+                </li>
+            )}
+        </ol>
+    );
+}
+
+function SubDefinition({ subDefinition }) {
+    if (subDefinition === null) {
+        return null;
+    }
+
+    return (
+        <div className={styles.subDefinitionContainer}>
+            <div className={styles.subDefinition}>
+                {subDefinition.definition}
+            </div>
+            <div className={styles.subDefinitionExamples}>
+                {subDefinition.examples}
+            </div>
+        </div>
+    )
+}
+
+function SubEntries({ subEntries }) {
+    if (subEntries.length === 0) {
+        return null;
+    }
+
+    return (
+        <ul className={styles.subEntriesList}>
+            {.subEntries.map(subEntry =>
+                <li>
+                    <span className={styles.subEntryTerm}>
+                        {subEntry.term}
+                    </span>
+                    {" "}
+                    {subEntry.definition}
+                </li>
+            )}
+        </ul>
+    );
 }
 
 export default Ordbok;

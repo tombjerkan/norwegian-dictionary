e56958d4f5cc1294c24520361f57f4669297a257
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-07-26 17:59:41 +0100

    Test content is shown for ordbok and google translate

--- testcafe.js
+++ testcafe.js
@@ -7,6 +7,13 @@ test("Can search for word and find definition", async t => {
         .typeText("input[type='text']", "nordmann")
         .click("input[type='submit']")
         .click(
+            Selector("#ordbok")
+                .find("button")
+                .withText("Show")
+        )
+        .expect(Selector("#ordbok").innerText)
+        .contains("person fra Norge")
+        .click(
             Selector("#wiktionary")
                 .find("button")
                 .withText("Show")
@@ -14,5 +21,12 @@ test("Can search for word and find definition", async t => {
         .expect(Selector("#wiktionary").innerText)
         .contains(
             "a Norwegian (person of Norwegian ancestry or inhabitant of Norway)"
-        );
+        )
+        .click(
+            Selector("#google-translate")
+                .find("button")
+                .withText("Show")
+        )
+        .expect(Selector("#google-translate").innerText)
+        .contains("Norwegian");
 });

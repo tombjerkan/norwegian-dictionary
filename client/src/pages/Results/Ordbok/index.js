5342222fa0781178646328844a9b53a0d2125048
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-07-29 22:19:00 +0100

    Show links in client

--- client/src/pages/Results/Ordbok/index.js
+++ client/src/pages/Results/Ordbok/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Section from "pages/Results/Section";
 import useFetch from "pages/Results/useFetch";
 import styles from "./styles.module.css";
@@ -20,8 +21,12 @@ function Ordbok({ query }) {
 function Entry({ entry }) {
     return (
         <li>
-            <h3 className={styles.term}>{entry.term}</h3>
-            <div className={styles.etymology}>{entry.etymology}</div>
+            <h3 className={styles.term}>
+                <TextWithLinks text={entry.term} />
+            </h3>
+            <div className={styles.etymology}>
+                <TextWithLinks text={entry.etymology} />
+            </div>
             <Senses senses={entry.senses} />
         </li>
     );
@@ -36,8 +41,14 @@ function Senses({ senses }) {
         <ol className={styles.sensesList}>
             {senses.map(sense => (
                 <li>
-                    <div className={styles.definition}>{sense.definition}</div>
-                    <div className={styles.examples}>{sense.examples}</div>
+                    <div className={styles.definition}>
+                        <TextWithLinks text={sense.definition} />
+                    </div>
+
+                    <div className={styles.examples}>
+                        <TextWithLinks text={sense.examples} />
+                    </div>
+
                     <SubDefinition subDefinition={sense.subDefinition} />
                     <SubEntries subEntries={sense.subEntries} />
                 </li>
@@ -54,10 +65,10 @@ function SubDefinition({ subDefinition }) {
     return (
         <div className={styles.subDefinitionContainer}>
             <div className={styles.subDefinition}>
-                {subDefinition.definition}
+                <TextWithLinks text={subDefinition.definition} />
             </div>
             <div className={styles.subDefinitionExamples}>
-                {subDefinition.examples}
+                <TextWithLinks text={subDefinition.examples} />
             </div>
         </div>
     );
@@ -72,11 +83,44 @@ function SubEntries({ subEntries }) {
         <ul className={styles.subEntriesList}>
             {subEntries.map(subEntry => (
                 <li>
-                    <span className={styles.subEntryTerm}>{subEntry.term}</span>{" "}
-                    {subEntry.definition}
+                    <span className={styles.subEntryTerm}>
+                        <TextWithLinks text={subEntry.term} />
+                    </span>{" "}
+                    <TextWithLinks text={subEntry.definition} />
                 </li>
             ))}
         </ul>
+    );
+}
+
+function TextWithLinks({ text }) {
+    // Text content does not have root element, so would be invalid if dummy
+    // root not added
+    const parser = new DOMParser();
+    const xmlDocument = parser.parseFromString(
+        `<Root>${text}</Root>`,
+        "text/xml"
+    );
+    const textNodes = xmlDocument.childNodes[0].childNodes;
+
+    return (
+        <React.Fragment>
+            {Array.from(textNodes).map(node => {
+                if (node.nodeType === 1 && node.tagName === "Link") {
+                    return (
+                        <Link to={node.getAttribute("to")}>
+                            {node.textContent}
+                        </Link>
+                    );
+                } else if (node.nodeType === 1) {
+                    return "<Invalid element>";
+                } else if (node.nodeType === 3) {
+                    return node.textContent;
+                } else {
+                    return "";
+                }
+            })}
+        </React.Fragment>
     );
 }
 

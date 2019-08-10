cfd5a8d711ee8cc8a2583c15d32aa324a83b3060
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-08-10 23:10:57 +0100

    Use animation for loading

--- client/src/components/Loading/index.js
+++ client/src/components/Loading/index.js
@@ -1,8 +1,15 @@
 import React from "react";
+import classNames from "classnames";
 import styles from "./styles.module.css";
 
 function Loading() {
-    return <div className={styles.container}>Loading...</div>;
+    return (
+        <div className={styles.container}>
+            <div className={classNames(styles.circle, styles.circle1)} />
+            <div className={classNames(styles.circle, styles.circle2)} />
+            <div className={classNames(styles.circle, styles.circle3)} />
+        </div>
+    );
 }
 
 export default Loading;

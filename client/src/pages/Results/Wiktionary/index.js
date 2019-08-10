5c012ac0b0f460b2daccd051faa20a9a019ef2a6
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-08-10 17:29:37 +0100

    Refactor Wiktionary components

--- client/src/pages/Results/Wiktionary/index.js
+++ client/src/pages/Results/Wiktionary/index.js
@@ -28,55 +28,79 @@ function Entry({ index, entry }) {
         <li>
             <h3 className={styles.entryHeader}>Etymology {index + 1}</h3>
 
-            {entry.etymology && (
-                <p className={styles.etymology}>
-                    <TextWithLinks text={entry.etymology} />
-                </p>
-            )}
+            {entry.etymology && <Etymology etymology={entry.etymology} />}
 
             {entry.subEntries.map(subEntry => (
-                <React.Fragment>
-                    <div className={styles.description}>
-                        <div className={styles.type}>{subEntry.type}</div>
-                        <div className={styles.term}>
-                            <TextWithLinks text={subEntry.term} />
-                        </div>
-                    </div>
-
-                    <ol className={styles.sensesList}>
-                        {subEntry.senses.map(sense => (
-                            <li>
-                                <TextWithLinks text={sense.definition} />
-
-                                {sense.examples && (
-                                    <ul className={styles.examplesList}>
-                                        {sense.examples.map(example => (
-                                            <li>
-                                                <TextWithLinks text={example} />
-                                            </li>
-                                        ))}
-                                    </ul>
-                                )}
-                            </li>
-                        ))}
-                    </ol>
-                </React.Fragment>
+                <SubEntry subEntry={subEntry} />
             ))}
 
             {entry.synonyms.length > 0 && (
-                <React.Fragment>
-                    <h4 className={styles.subSectionHeader}>Synonyms</h4>
-                    <TextWithLinks text={entry.synonyms.join(", ")} />
-                </React.Fragment>
+                <WordList title="Synonyms" words={entry.synonyms} />
             )}
-
             {entry.derived.length > 0 && (
-                <React.Fragment>
-                    <h4 className={styles.subSectionHeader}>Derived terms</h4>
-                    <TextWithLinks text={entry.derived.join(", ")} />
-                </React.Fragment>
+                <WordList title="Derived terms" words={entry.derived} />
             )}
         </li>
+    );
+}
+
+function Etymology({ etymology }) {
+    return (
+        <p className={styles.etymology}>
+            <TextWithLinks text={etymology} />
+        </p>
+    );
+}
+
+function SubEntry({ subEntry }) {
+    return (
+        <React.Fragment>
+            <SubEntryHeader type={subEntry.type} term={subEntry.term} />
+
+            <ol className={styles.sensesList}>
+                {subEntry.senses.map(sense => (
+                    <Sense sense={sense} />
+                ))}
+            </ol>
+        </React.Fragment>
+    );
+}
+
+function SubEntryHeader({ type, term }) {
+    return (
+        <div className={styles.subEntryHeader}>
+            <div className={styles.type}>{type}</div>
+            <div className={styles.term}>
+                <TextWithLinks text={term} />
+            </div>
+        </div>
+    );
+}
+
+function Sense({ sense }) {
+    return (
+        <li>
+            <TextWithLinks text={sense.definition} />
+
+            {sense.examples && (
+                <ul className={styles.examplesList}>
+                    {sense.examples.map(example => (
+                        <li>
+                            <TextWithLinks text={example} />
+                        </li>
+                    ))}
+                </ul>
+            )}
+        </li>
+    );
+}
+
+function WordList({ title, words }) {
+    return (
+        <React.Fragment>
+            <h4 className={styles.wordListTitle}>{title}</h4>
+            <TextWithLinks text={words.join(", ")} />
+        </React.Fragment>
     );
 }
 

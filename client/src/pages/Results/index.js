642217d812ff3e590d52a0ffa3da8ba1f2ad88e2
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-09-29 09:30:20 +0100

    Add clickable sstar to results page

--- client/src/pages/Results/index.js
+++ client/src/pages/Results/index.js
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
 import styles from "./styles.module.css";
 import MaxWidthLimit from "components/MaxWidthLimit";
 import SearchBox from "components/SearchBox";
+import { ReactComponent as Star } from "./star.svg";
 import Ordbok from "./Ordbok";
 import Wiktionary from "./Wiktionary";
 import GoogleTranslate from "./GoogleTranslate";
 
 export default function Results({ history, match }) {
+    const [isStarred, setStarred] = useState(false);
+
     return (
         <div>
             <nav className={styles.searchBar}>
                 <MaxWidthLimit>
-                    <SearchBox history={history} className={styles.searchBox} />
+                    <div className={styles.searchBoxContainer}>
+                        <SearchBox
+                            history={history}
+                            className={styles.searchBox}
+                        />
+                        <Star
+                            onClick={() => {
+                                setStarred(true);
+                                window.localStorage.setItem(
+                                    match.params.query,
+                                    true
+                                );
+                            }}
+                            className={classNames(
+                                styles.star,
+                                isStarred && styles.filled
+                            )}
+                        />
+                    </div>
                 </MaxWidthLimit>
             </nav>
 

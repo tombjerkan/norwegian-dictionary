38dbdb86ff64cb0a8df03dc580d8b51875aeaf97
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-10-02 16:55:39 +0100

    Move star button to floating button at bottom of screen

---
+++ client/src/pages/Results/Star/index.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import classNames from "classnames";
+import { ReactComponent as StarIcon } from "../star.svg";
+import styles from "./styles.module.css";
+
+export default function Star({ query }) {
+    const [isStarred, setStarred] = useState(false);
+    const [isEditingNotes, setEditingNotes] = useState(false);
+    const [notes, setNotes] = useState("");
+
+    useEffect(() => {
+        const storageEntry = window.localStorage.getItem(query);
+        if (storageEntry === null) {
+            setStarred(false);
+            setNotes("");
+        } else {
+            setStarred(true);
+            setNotes(storageEntry);
+        }
+    }, [query]);
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.starButton}>
+                <StarIcon
+                    onClick={() => {
+                        setStarred(true);
+                        setEditingNotes(true);
+                    }}
+                    className={classNames(
+                        styles.star,
+                        isStarred && styles.filled
+                    )}
+                />
+            </div>
+
+            {isEditingNotes && (
+                <div className={styles.notesBubble}>
+                    <textarea
+                        className={styles.notes}
+                        value={notes}
+                        placeholder="Enter notes..."
+                        onChange={event => setNotes(event.target.value)}
+                    />
+
+                    <button
+                        className={styles.cancel}
+                        onClick={() => {
+                            setStarred(false);
+                            setEditingNotes(false);
+                        }}
+                    >
+                        Cancel
+                    </button>
+
+                    <button
+                        className={styles.ok}
+                        onClick={() => {
+                            setEditingNotes(false);
+                            window.localStorage.setItem(query, notes);
+                        }}
+                    >
+                        Ok
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+}
--- client/src/pages/Results/index.js
+++ client/src/pages/Results/index.js
@@ -1,30 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import classNames from "classnames";
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 import MaxWidthLimit from "components/MaxWidthLimit";
 import SearchBox from "components/SearchBox";
-import { ReactComponent as Star } from "./star.svg";
 import Ordbok from "./Ordbok";
 import Wiktionary from "./Wiktionary";
 import GoogleTranslate from "./GoogleTranslate";
+import Star from "./Star";
 
 export default function Results({ history, match }) {
-    const [isStarred, setStarred] = useState(false);
-    const [isEditingNotes, setEditingNotes] = useState(false);
-    const [notes, setNotes] = useState("");
-
-    useEffect(() => {
-        const storageEntry = window.localStorage.getItem(match.params.query);
-        if (storageEntry === null) {
-            setStarred(false);
-            setNotes("");
-        } else {
-            setStarred(true);
-            setNotes(storageEntry);
-        }
-    }, [match.params.query]);
-
     return (
         <div>
             <nav className={styles.navigationBar}>
@@ -40,58 +25,7 @@ export default function Results({ history, match }) {
                         </Link>
                     </div>
 
-                    <div className={styles.searchBoxContainer}>
-                        <SearchBox
-                            history={history}
-                            className={styles.searchBox}
-                        />
-                        <Star
-                            onClick={() => {
-                                setStarred(true);
-                                setEditingNotes(true);
-                            }}
-                            className={classNames(
-                                styles.star,
-                                isStarred && styles.filled
-                            )}
-                        />
-
-                        {isEditingNotes && (
-                            <div className={styles.notesBubble}>
-                                <textarea
-                                    className={styles.notes}
-                                    value={notes}
-                                    placeholder="Enter notes..."
-                                    onChange={event =>
-                                        setNotes(event.target.value)
-                                    }
-                                />
-
-                                <button
-                                    className={styles.cancel}
-                                    onClick={() => {
-                                        setStarred(false);
-                                        setEditingNotes(false);
-                                    }}
-                                >
-                                    Cancel
-                                </button>
-
-                                <button
-                                    className={styles.ok}
-                                    onClick={() => {
-                                        setEditingNotes(false);
-                                        window.localStorage.setItem(
-                                            match.params.query,
-                                            notes
-                                        );
-                                    }}
-                                >
-                                    Ok
-                                </button>
-                            </div>
-                        )}
-                    </div>
+                    <SearchBox history={history} className={styles.searchBox} />
                 </MaxWidthLimit>
             </nav>
 
@@ -101,6 +35,8 @@ export default function Results({ history, match }) {
                     <Wiktionary query={match.params.query} />
                     <Ordbok query={match.params.query} />
                 </div>
+
+                <Star query={match.params.query} />
             </MaxWidthLimit>
         </div>
     );

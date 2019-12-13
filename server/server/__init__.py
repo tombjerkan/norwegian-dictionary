3bc23fa97bbcbe81ba31b2e6df57271098a7c12f
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-12-13 19:12:15 +0000

    Add Sentry to Python server

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,8 +1,18 @@
 from dotenv import load_dotenv
 from flask import Flask
+import os
+import sentry_sdk
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 app = Flask(__name__)
 load_dotenv()
+
+
+if os.environ["FLASK_ENV"] == "production":
+    sentry_sdk.init(
+        dsn="https://e4c0973426a3496296ee7a2edafb7e24@sentry.io/1764489",
+        integrations=[FlaskIntegration()],
+    )
 
 
 class ApiError(Exception):

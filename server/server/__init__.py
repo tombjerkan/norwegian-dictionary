1cd16e24f6348ccfcd70455d75d7de4a8fa2de74
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-12-13 15:21:32 +0000

    Write Google Translate route in Python

--- server/server/__init__.py
+++ server/server/__init__.py
@@ -1,6 +1,8 @@
+from dotenv import load_dotenv
 from flask import Flask
 
 app = Flask(__name__)
+load_dotenv()
 
 
 class ApiError(Exception):
--- server/server/googletranslate.py
+++ server/server/googletranslate.py
@@ -1,6 +1,28 @@
-from server import app
+import google.oauth2
+from google.cloud import translate_v2 as translate
+import os
+
+from server import app, ApiError
 
 
-@app.route("/googleTranslate")
-def google_translate():
-    return {}
+@app.route("/googleTranslate/<word>")
+def google_translate(word):
+    service_account_info = {
+        "token_uri": "https://oauth2.googleapis.com/token",
+        "client_email": os.getenv("GOOGLE_AUTH_CLIENT_EMAIL"),
+        "private_key": os.getenv("GOOGLE_AUTH_PRIVATE_KEY")
+    }
+
+    try:
+        credentials = google.oauth2.service_account.Credentials.from_service_account_info(service_account_info)
+    except ValueError:
+        raise ApiError(500)
+
+    client = translate.Client(credentials=credentials)
+
+    try:
+        response = client.translate(word, source_language="no")
+    except google.auth.exceptions.RefreshError:
+        raise ApiError(500)
+
+    return response["translatedText"]

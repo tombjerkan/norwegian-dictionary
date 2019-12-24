26b3100a6383c47adf85366db67f10a79cfeedab
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-12-24 11:34:20 +0000

    Add route for individual starred entry

--- server/server/starred.py
+++ server/server/starred.py
@@ -15,14 +15,15 @@ class StarredSchema(marshmallow.Schema):
         fields = ("term", "notes")
 
 
-starred_schema = StarredSchema(many=True)
+starred_schema = StarredSchema()
+starred_schema_many = StarredSchema(many=True)
 
 
 @app.route("/api/starred", methods=["GET", "POST"])
 def starred():
     if flask.request.method == "POST":
-        term = flask.request.values.get("term")
-        notes = flask.request.values.get("notes")
+        term = flask.request.json.get("term")
+        notes = flask.request.json.get("notes")
 
         starred = Starred.query.get(term)
         if starred:
@@ -36,4 +37,18 @@ def starred():
         return "", 200
 
     else:
-        return flask.jsonify(starred_schema.dump(Starred.query.all()))
+        return flask.jsonify(starred_schema_many.dump(Starred.query.all()))
+
+
+@app.route("/api/starred/<word>", methods=["GET", "DELETE"])
+def one_starred(word):
+    starred = Starred.query.get(word)
+    if not starred:
+        return "", 404
+
+    if flask.request.method == "GET":
+        return starred_schema.dump(starred)
+    else:
+        db.session.delete(starred)
+        db.session.commit()
+        return "", 200

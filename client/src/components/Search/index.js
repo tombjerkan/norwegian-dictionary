c2764ce8f0d93d08d70801718af710ef3e815a8b
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-12-26 15:24:52 +0000

    Redesign search box component

---
+++ client/src/components/Search/index.js
@@ -0,0 +1,40 @@
+import React, { useState, useRef } from "react";
+import classNames from "classnames";
+import { ReactComponent as Magnifier } from "./Magnifier.svg";
+import styles from "./styles.module.css";
+
+export default function Search({ initialValue, onSubmit, className }) {
+    const [value, setValue] = useState(initialValue || "");
+    const inputRef = useRef(null);
+
+    function handleChange(event) {
+        setValue(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        // Input does not automatically unfocus as same root page is used,
+        // must unfocus manually for mobile keyboard to disappear.
+        inputRef.current.blur();
+
+        onSubmit(value);
+        event.preventDefault();
+    }
+
+    return (
+        <form
+            onSubmit={handleSubmit}
+            className={classNames(styles.container, className)}
+        >
+            <Magnifier className={styles.magnifier} />
+
+            <input
+                type="text"
+                placeholder="Search..."
+                value={value}
+                onChange={handleChange}
+                ref={inputRef}
+                className={styles.input}
+            />
+        </form>
+    );
+}

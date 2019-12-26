8c7ce717c9481e61c9c1148b1bc0e94324535e11
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-12-26 21:56:17 +0000

    Use same page for initial and following searches

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -1,13 +1,6 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
 import Search from "./Search";
-import Results from "./Results";
 
 export default function App() {
-    return (
-        <BrowserRouter>
-            <Route exact path="/" component={Search} />
-            <Route path="/:query" component={Results} />
-        </BrowserRouter>
-    );
+    return <Search />;
 }

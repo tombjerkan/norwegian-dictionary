5c53ea22ea36bbc9664a43a478b0be45bc4ba39d
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-12-27 12:15:08 +0000

    Remove unused imports

--- client/src/components/Search/stories.js
+++ client/src/components/Search/stories.js
@@ -2,7 +2,6 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import Search from ".";
-import { ReactComponent as Magnifier } from "./Magnifier.svg";
 
 storiesOf("Search", module).add("default", () => (
     <Search onSubmit={action("onSubmit")} />

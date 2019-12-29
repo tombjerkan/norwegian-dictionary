bc481e0480884a42919c0bc736400fcc5232acd2
Thomas Bjerkan <t.bjerkan@gmail.com>
2019-12-29 18:49:19 +0000

    Split StarredPage into several components

--- client/src/pages/StarredPage/index.js
+++ client/src/pages/StarredPage/index.js
@@ -38,33 +38,49 @@ export function StarredPageView({ entries, onDelete }) {
             </Navigation>
 
             <MaxWidthLimit>
-                <div className={styles.starred}>
-                    <h2>Starred</h2>
-
-                    <ul className={styles.entries}>
-                        {entries.map(entry => (
-                            <li className={styles.entry}>
-                                <h3 className={styles.term}>
-                                    <Link to={`/search/${entry.term}`}>
-                                        {entry.term}
-                                    </Link>
-                                </h3>
-
-                                <button
-                                    onClick={() => {
-                                        onDelete(entry.term);
-                                    }}
-                                    className={styles.removeButton}
-                                >
-                                    Remove
-                                </button>
-
-                                <p className={styles.notes}>{entry.notes}</p>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                <StarredEntries>
+                    {entries.map(entry => (
+                        <StarredEntry
+                            term={entry.term}
+                            notes={entry.notes}
+                            onDelete={() => onDelete(entry.term)}
+                        />
+                    ))}
+                </StarredEntries>
             </MaxWidthLimit>
         </div>
+    );
+}
+
+function StarredEntries({ children }) {
+    return (
+        <div className={styles.starred}>
+            <h2 className={styles.header}>Starred</h2>
+
+            <ul className={styles.entries}>
+                {children}
+            </ul>
+        </div>
+    );
+}
+
+function StarredEntry({ term, notes, onDelete }) {
+    return (
+        <li className={styles.entry}>
+            <h3 className={styles.term}>
+                <Link to={`/search/${term}`}>
+                    {term}
+                </Link>
+            </h3>
+
+            <button
+                onClick={onDelete}
+                className={styles.removeButton}
+            >
+                Remove
+            </button>
+
+            <p className={styles.notes}>{notes}</p>
+        </li>
     );
 }

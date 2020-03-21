9ceb020a670206bd7d48e13bc40a41e13fe53677
Thomas Bjerkan <thomas.bjerkan@bynder.com>
2020-03-21 14:23:56 +0100

    Use new Section for Wiktionary

--- client/src/pages/SearchPage/Wiktionary/index.js
+++ client/src/pages/SearchPage/Wiktionary/index.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
+import classNames from "classnames";
+import Section from "components/Section";
 import Text from "components/Text";
+import Loading from "components/Loading";
+import { ReactComponent as Error } from "components/Error.svg";
+import { ReactComponent as Chevron } from "components/Chevron.svg";
 import useFetch from "../useFetch";
-import { ExpandableSection } from "../Section";
 import {
     Entry,
     Header,
@@ -17,69 +21,134 @@ import styles from "./styles.module.css";
 export default function WiktionaryContainer({ query }) {
     const [data, isLoading, error] = useFetch(`/api/wiktionary/${query}`);
 
-    return <WiktionaryView data={data} isLoading={isLoading} error={error} key={query} />;
+    return (
+        <WiktionaryView
+            data={data}
+            isLoading={isLoading}
+            error={error}
+            key={query}
+        />
+    );
 }
 
 export function WiktionaryView({ data, isLoading, error }) {
+    const [isOpen, setOpen] = useState(false);
+
+    function toggleOpen() {
+        setOpen(prev => !prev);
+    }
+
+    function close() {
+        setOpen(false);
+    }
+
+    const isNotFound = !isLoading && error === 404;
+    const isError = !isLoading && error !== null && error !== 404;
+    const isContentAvailable = !isLoading && error === null;
+
     return (
-        <ExpandableSection
-            title="Wiktionary"
-            isLoading={isLoading}
-            error={error}
-        >
-            {data &&
-                data.map((entry, index) => (
-                    <Entry>
-                        <Header>Etymology {index + 1}</Header>
+        <Section isAvailable={isContentAvailable || isLoading}>
+            <div onClick={toggleOpen} className={styles.header}>
+                <h2 className={styles.title}>Wiktionary</h2>
 
-                        <Etymology>
-                            <Text text={entry.etymology} />
-                        </Etymology>
+                {isLoading && <Loading />}
+                {isNotFound && <div>Not available</div>}
+                {isError && <Error style={{ height: "30px" }} />}
+                {isContentAvailable && (
+                    <Chevron
+                        className={classNames(
+                            styles.chevron,
+                            isOpen && styles.closeChevron
+                        )}
+                    />
+                )}
+            </div>
 
-                        {entry.subEntries.map(subEntry => (
-                            <>
-                                <SubHeader>{subEntry.type}</SubHeader>
+            {isContentAvailable && isOpen && (
+                <>
+                    <div className={styles.expandableContent}>
+                        {data &&
+                            data.map((entry, index) => (
+                                <Entry>
+                                    <Header>Etymology {index + 1}</Header>
 
-                                <Paragraph>
-                                    <Text text={subEntry.term} />
-                                </Paragraph>
+                                    <Etymology>
+                                        <Text text={entry.etymology} />
+                                    </Etymology>
 
-                                <SenseList>
-                                    {subEntry.senses.map(sense => (
-                                        <Sense>
-                                            <Text text={sense.definition} />
-                                            {sense.examples.map(example => (
-                                                <Examples
-                                                    className={styles.examples}
-                                                >
-                                                    <Text text={example} />
-                                                </Examples>
-                                            ))}
-                                        </Sense>
+                                    {entry.subEntries.map(subEntry => (
+                                        <>
+                                            <SubHeader>
+                                                {subEntry.type}
+                                            </SubHeader>
+
+                                            <Paragraph>
+                                                <Text text={subEntry.term} />
+                                            </Paragraph>
+
+                                            <SenseList>
+                                                {subEntry.senses.map(sense => (
+                                                    <Sense>
+                                                        <Text
+                                                            text={
+                                                                sense.definition
+                                                            }
+                                                        />
+                                                        {sense.examples.map(
+                                                            example => (
+                                                                <Examples
+                                                                    className={
+                                                                        styles.examples
+                                                                    }
+                                                                >
+                                                                    <Text
+                                                                        text={
+                                                                            example
+                                                                        }
+                                                                    />
+                                                                </Examples>
+                                                            )
+                                                        )}
+                                                    </Sense>
+                                                ))}
+                                            </SenseList>
+                                        </>
                                     ))}
-                                </SenseList>
-                            </>
-                        ))}
 
-                        {entry.synonyms.length > 0 && (
-                            <>
-                                <SubHeader>Synonyms</SubHeader>
-                                <Paragraph>
-                                    <Text text={entry.synonyms.join(", ")} />
-                                </Paragraph>
-                            </>
-                        )}
+                                    {entry.synonyms.length > 0 && (
+                                        <>
+                                            <SubHeader>Synonyms</SubHeader>
+                                            <Paragraph>
+                                                <Text
+                                                    text={entry.synonyms.join(
+                                                        ", "
+                                                    )}
+                                                />
+                                            </Paragraph>
+                                        </>
+                                    )}
 
-                        {entry.derived.length > 0 && (
-                            <>
-                                <SubHeader>Derived</SubHeader>
-                                <Paragraph>
-                                    <Text text={entry.derived.join(", ")} />
-                                </Paragraph>
-                            </>
-                        )}
-                    </Entry>
-                ))}
-        </ExpandableSection>
+                                    {entry.derived.length > 0 && (
+                                        <>
+                                            <SubHeader>Derived</SubHeader>
+                                            <Paragraph>
+                                                <Text
+                                                    text={entry.derived.join(
+                                                        ", "
+                                                    )}
+                                                />
+                                            </Paragraph>
+                                        </>
+                                    )}
+                                </Entry>
+                            ))}
+                    </div>
+
+                    <button onClick={close} className={styles.closeButton}>
+                        <Chevron className={styles.buttonChevron} />
+                    </button>
+                </>
+            )}
+        </Section>
     );
 }

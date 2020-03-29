e397a19731d94bb1c0c360d1a8944be74f011702
Thomas Bjerkan <thomas.bjerkan@bynder.com>
2020-03-29 21:20:35 +0100

    Separate styles wanted in stories and styles only for app

--- client/src/pages/App.js
+++ client/src/pages/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useLocation } from "routing";
 import SearchPage from "./SearchPage";
 import StarredPage from "./StarredPage";
+import "./App.css";
 
 export default function App() {
     const location = useLocation();

3f4c699c39ece3540d422e5725f64e45ead40a51
Thomas Bjerkan <thomas.bjerkan@bynder.com>
2020-04-05 13:14:02 +0100

    Render ordbok content using sanitised HTML

---
+++ client/src/components/OrdbokContent/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import DomPurify from "dompurify";
+
+import "./styles.css";
+
+export default function OrdbokContent({ data }) {
+    const sanitisedData = data.map(entry => ({
+        term: entry.term,
+        content: DomPurify.sanitize(entry.content)
+    }));
+
+    return sanitisedData.map(entry => (
+        <>
+            <h1>{entry.term}</h1>
+            <div dangerouslySetInnerHTML={{ __html: entry.content }} />
+        </>
+    ));
+}
--- client/src/pages/SearchPage/Ordbok/index.js
+++ client/src/pages/SearchPage/Ordbok/index.js
@@ -1,21 +1,11 @@
 import React, { useState } from "react";
 import Section from "components/Section";
-import Text from "components/Text";
+import OrdbokContent from "components/OrdbokContent";
 import ExpandChevron from "components/ExpandChevron";
 import Loading from "components/Loading";
 import { ReactComponent as Error } from "components/Error.svg";
 import CloseButton from "components/CloseButton";
 import useFetch from "../useFetch";
-import {
-    Entry,
-    Header,
-    SubHeader,
-    Etymology,
-    SenseList,
-    Sense,
-    Paragraph,
-    Examples
-} from "../common";
 import styles from "./styles.module.css";
 
 export default function OrdbokContainer({ query }) {
@@ -60,90 +50,12 @@ export function OrdbokView({ data, isLoading, error }) {
             {isContentAvailable && isOpen && (
                 <>
                     <div className={styles.expandableContent}>
-                        {data &&
-                            data.map(entry => (
-                                <Entry>
-                                    <Header>{entry.term}</Header>
-
-                                    <Etymology>
-                                        <Text text={entry.etymology} />
-                                    </Etymology>
-
-                                    <SenseList>
-                                        {entry.senses.map(sense => (
-                                            <Sense>
-                                                <SubHeader>
-                                                    <Text
-                                                        text={sense.definition}
-                                                    />
-                                                </SubHeader>
-
-                                                <Examples>
-                                                    <Text
-                                                        text={sense.examples}
-                                                    />
-                                                </Examples>
-
-                                                {sense.subDefinitions.map(
-                                                    subDefinition => (
-                                                        <SubDefinition
-                                                            definition={
-                                                                subDefinition.definition
-                                                            }
-                                                            examples={
-                                                                subDefinition.examples
-                                                            }
-                                                        />
-                                                    )
-                                                )}
-
-                                                {sense.subEntries.map(
-                                                    subEntry => (
-                                                        <SubEntry
-                                                            term={subEntry.term}
-                                                            definition={
-                                                                subEntry.definition
-                                                            }
-                                                        />
-                                                    )
-                                                )}
-                                            </Sense>
-                                        ))}
-                                    </SenseList>
-                                </Entry>
-                            ))}
+                        {data && <OrdbokContent data={data} />}
                     </div>
 
                     <CloseButton onClose={close} />
                 </>
             )}
         </Section>
-    );
-}
-
-function SubDefinition({ definition, examples }) {
-    return (
-        <div className={styles.subDefinition}>
-            <SubHeader>
-                <Text text={definition} />
-            </SubHeader>
-
-            <Examples>
-                <Text text={examples} />
-            </Examples>
-        </div>
-    );
-}
-
-function SubEntry({ term, definition }) {
-    return (
-        <Paragraph>
-            <span className={styles.subEntryTerm}>
-                <Text text={term} />
-            </span>{" "}
-            <span className={styles.subEntryDefinition}>
-                <Text text={definition} />
-            </span>
-        </Paragraph>
     );
 }

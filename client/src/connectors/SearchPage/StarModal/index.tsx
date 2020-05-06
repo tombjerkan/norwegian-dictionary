7914e835e5664f22a63f301605252b8b1c3b53f9
Thomas Bjerkan <thomas.bjerkan@bynder.com>
2020-05-06 20:37:05 +0100

    Use Tailwind for StarModal

--- client/src/connectors/SearchPage/StarModal/index.tsx
+++ client/src/connectors/SearchPage/StarModal/index.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import classNames from "classnames";
-import styles from "./styles.module.css";
 
 interface Props {
     initialNotes?: string;
@@ -19,27 +17,28 @@ export default function StarModal({ initialNotes = "", ...props }: Props) {
     return ReactDOM.createPortal(
         <div className="fixed flex justify-center items-center inset-0 bg-gray-800 bg-opacity-50 z-10">
             <div
-                className={classNames(
-                    "bg-white p-8 rounded-lg",
-                    styles.innerContainer
-                )}
+                className={
+                    "w-120 bg-white p-8 rounded-lg grid grid-columns-buttons-2 grid-rows-button-modal gap-6"
+                }
             >
                 <textarea
-                    className={classNames(
-                        "rounded border resize-none p-3 h-40",
-                        styles.notes
-                    )}
+                    className={
+                        "rounded border resize-none p-3 h-40 col-start-1 col-end-4 row-start-1 row-end-2"
+                    }
                     value={notes}
                     placeholder="Enter notes..."
                     onChange={event => setNotes(event.target.value)}
                 />
 
-                <button className={styles.cancel} onClick={props.onCancel}>
+                <button
+                    className="col-start-2 col-end-3 row-start-2 row-end-3"
+                    onClick={props.onCancel}
+                >
                     Cancel
                 </button>
 
                 <button
-                    className={styles.ok}
+                    className="col-start-3 col-end-4 row-start-2 row-end-3"
                     onClick={() => props.onSave(notes)}
                 >
                     Ok
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
     purge: [],
     theme: {
-        extend: {}
+        extend: {
+            gridTemplateColumns: {
+                "buttons-2": "auto 80px 80px"
+            },
+            gridTemplateRows: {
+                "button-modal": "auto 32px"
+            },
+            spacing: {
+                "120": "30rem"
+            }
+        }
     },
     variants: {},
     plugins: []

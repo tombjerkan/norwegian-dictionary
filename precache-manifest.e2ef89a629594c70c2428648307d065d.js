81e4600519893d46b2e14b063fec83ec754854c8
Thomas Bjerkan <thomas.bjerkan@bynder.com>
2021-01-31 21:55:00 +0100

    Updates

---
+++ precache-manifest.e2ef89a629594c70c2428648307d065d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b99e479e4c296a44a2016ed4f37a0fc",
+    "url": "/norwegian-dictionary/index.html"
+  },
+  {
+    "revision": "b85d9a8c1f3e32357795",
+    "url": "/norwegian-dictionary/static/css/main.fb965d66.chunk.css"
+  },
+  {
+    "revision": "dbc68bd0660daca77d08",
+    "url": "/norwegian-dictionary/static/js/2.2bc98f92.chunk.js"
+  },
+  {
+    "revision": "717a8e98592f8b5568161604c63949df",
+    "url": "/norwegian-dictionary/static/js/2.2bc98f92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b85d9a8c1f3e32357795",
+    "url": "/norwegian-dictionary/static/js/main.e5aaa2b3.chunk.js"
+  },
+  {
+    "revision": "f298603e98893fe19023",
+    "url": "/norwegian-dictionary/static/js/runtime-main.ee2655c6.js"
+  },
+  {
+    "revision": "b4eb61730eb95f921b86d9570630a3a4",
+    "url": "/norwegian-dictionary/static/media/Chevron.b4eb6173.svg"
+  },
+  {
+    "revision": "8c7770eb8b3e6e7a95d0016587088a79",
+    "url": "/norwegian-dictionary/static/media/Error.8c7770eb.svg"
+  },
+  {
+    "revision": "79260e5b693fac2ee373b659f01a2bdf",
+    "url": "/norwegian-dictionary/static/media/Inter-Bold.79260e5b.woff"
+  },
+  {
+    "revision": "aed27700d84e327fda56b4a427b03061",
+    "url": "/norwegian-dictionary/static/media/Inter-Bold.aed27700.woff2"
+  },
+  {
+    "revision": "8ef77a031c5b96e53f2eeeb009232181",
+    "url": "/norwegian-dictionary/static/media/Inter-BoldItalic.8ef77a03.woff2"
+  },
+  {
+    "revision": "e0879d64763e509f309940120e7d1f5a",
+    "url": "/norwegian-dictionary/static/media/Inter-BoldItalic.e0879d64.woff"
+  },
+  {
+    "revision": "9528384cf7aebfa81a198626cb05f5e8",
+    "url": "/norwegian-dictionary/static/media/Inter-Italic.9528384c.woff2"
+  },
+  {
+    "revision": "e4ad3666e223c64e4df963cec740bf09",
+    "url": "/norwegian-dictionary/static/media/Inter-Italic.e4ad3666.woff"
+  },
+  {
+    "revision": "4dd66a113d54a7f9a1ae913049610617",
+    "url": "/norwegian-dictionary/static/media/Inter-Regular.4dd66a11.woff2"
+  },
+  {
+    "revision": "7c539936c4c8c822b59a1bcc6c08a6ec",
+    "url": "/norwegian-dictionary/static/media/Inter-Regular.7c539936.woff"
+  },
+  {
+    "revision": "c0413def9f79a6551aa4e43044963d83",
+    "url": "/norwegian-dictionary/static/media/Magnifier.c0413def.svg"
+  },
+  {
+    "revision": "283771385c1a2cf59e61ba46ec19d926",
+    "url": "/norwegian-dictionary/static/media/Star.28377138.svg"
+  }
+]);

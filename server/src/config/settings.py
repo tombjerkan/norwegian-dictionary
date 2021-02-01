65cb5d95150f39e3316c24a89ac29c3d01c923c6
Thomas Bjerkan <thomas.bjerkan@bynder.com>
2021-02-01 17:36:04 +0100

    Get SECRET_KEY from environment

--- server/src/config/settings.py
+++ server/src/config/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@nq*!wb1ee++jd85r_%7)*eh#o3ebk5(dhf=bwg9qx)fr@=f2k'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

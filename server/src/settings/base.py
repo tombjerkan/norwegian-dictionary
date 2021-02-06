ff6e1f78c16ac1747af473939dfced6c58659ab6
Thomas Bjerkan <thomas.bjerkan@bynder.com>
2021-02-06 13:39:50 +0100

    Split settings files

--- server/src/settings/base.py
+++ server/src/settings/base.py
@@ -23,16 +23,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = [
-    'localhost',
-    'https://nordict.netlify.app',
-    'nordict.herokuapp.com'
-]
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -138,5 +128,3 @@ GOOGLE_AUTH = {
     "CLIENT_EMAIL": os.getenv("GOOGLE_AUTH_CLIENT_EMAIL"),
     "PRIVATE_KEY": os.getenv("GOOGLE_AUTH_PRIVATE_KEY"),
 }
-
-CORS_ORIGIN_WHITELIST = ['https://nordict.netlify.app']
---
+++ server/src/settings/production.py
@@ -0,0 +1,9 @@
+from .base import *
+
+
+ALLOWED_HOSTS = [
+    'https://nordict.netlify.app',
+    'nordict.herokuapp.com'
+]
+
+CORS_ORIGIN_WHITELIST = ['https://nordict.netlify.app']

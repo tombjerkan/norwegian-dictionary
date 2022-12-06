ac262f17b157ef7c63216bc5d507b756f9f388c8
Tom <t.bjerkan@gmail.com>
2022-12-06 16:37:33 +0100

    Remove unused starred functionality

--- client/src/connectors/App.tsx
+++ client/src/connectors/App.tsx
@@ -1,19 +1,11 @@
 import React from "react";
-import { useLocation } from "routing";
 import TailwindThemeProvider from "../tailwind/TailwindThemeProvider";
 import SearchPage from "./SearchPage";
-import StarredPage from "./StarredPage";
 
 export default function App() {
-    const pathName = useLocation();
-
     return (
         <TailwindThemeProvider>
-            {pathName === "/" && <SearchPage query="" />}
-            {pathName.startsWith("/search/") && (
-                <SearchPage query={pathName.slice(8)} />
-            )}
-            {pathName === "/starred" && <StarredPage />}
+            <SearchPage  />
         </TailwindThemeProvider>
     );
 }
